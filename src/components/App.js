892f9ad36ae3fadf6646d8779b3eb67b599bf6f8
Marianela Landoni Herrera <mlandonih@hotmail.com>
2022-08-24 12:57:10 +0200

    Add landing, header link

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import photo from '../images/photo2.png';
 import { useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import knife from '../images/knife.png';
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -19,63 +20,69 @@ import Button from './Button';
 
 import Share from './Share';
 import Card from './Card';
+import Landing from './Landing';
 
 function App() {
-  
   //Variables de estado
-  const [dataCard, setDataCard] = useState(ls.get('data', {
-    palette: '1',
-    name: '',
-    job: '',
-    phone: '',
-    email: '',
-    linkedin: '',
-    github: '',
-    photo: ''
-  }));
+  const [dataCard, setDataCard] = useState(
+    ls.get('data', {
+      palette: '1',
+      name: '',
+      job: '',
+      phone: '',
+      email: '',
+      linkedin: '',
+      github: '',
+      photo: '',
+    })
+  );
   const [resultCard, setResultCard] = useState({});
-  
 
   //Image
-  
 
-//Recoger el valor de los inputs
-const handleInput = (inputName, inputValue) => {
-    
-  setDataCard({
-    ...dataCard, [inputName]: inputValue
-  });
-
-}
-
+  //Recoger el valor de los inputs
+  const handleInput = (inputName, inputValue) => {
+    setDataCard({
+      ...dataCard,
+      [inputName]: inputValue,
+    });
+  };
 
   // Crear tarjeta (API)
   const handleCreateCard = () => {
-    dataApi(dataCard).then(info => {
+    dataApi(dataCard).then((info) => {
       setResultCard(info);
-    })
-  }
+    });
+  };
 
-  
   //Para los colapsables
   const handleCollapsed = (ev) => {
-    
     console.log(ev.currentTarget);
-  }
+  };
 
   ls.set('data', dataCard);
-  
+
   return (
     <div>
-      
-      <Card handleCollapsed={handleCollapsed} dataCard={dataCard} setDataCard={setDataCard} resultCard={resultCard} setResultCard={setResultCard} handleInput={handleInput} handleCreateCard={handleCreateCard} />
-      
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route
+          path="/card"
+          element={
+            <Card
+              handleCollapsed={handleCollapsed}
+              dataCard={dataCard}
+              setDataCard={setDataCard}
+              resultCard={resultCard}
+              setResultCard={setResultCard}
+              handleInput={handleInput}
+              handleCreateCard={handleCreateCard}
+            />
+          }
+        />
+      </Routes>
     </div>
   );
 }
-
-
-
-
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
 import logo from '../images/scream-team-04.png';
 import '../styles/layout/header.scss';
+import { Link } from 'react-router-dom';
 
-function Header () {
-    return (<header className="header__profile">
-    <a className="header__profile__link" href="./index.html" title="Volver a la página principal">
-      <img className="header__profile__logo" src={logo} alt="Logo Awesome Profile Cards" title="Logo Awesome Profile Cards" />
-    </a>
-  </header>);
-};
+function Header() {
+  return (
+    <header className="header__profile">
+      <Link
+        className="header__profile__link"
+        to="/"
+        title="Volver a la página principal"
+      >
+        <img
+          className="header__profile__logo"
+          src={logo}
+          alt="Logo Awesome Profile Cards"
+          title="Logo Awesome Profile Cards"
+        />
+      </Link>
+    </header>
+  );
+}
 
 export default Header;
